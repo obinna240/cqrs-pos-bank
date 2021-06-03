9b8feb2b4f20e8433a34141bfe64d45166f5fecb
Obinna Onyimadu <obinna240@yahoo.co.uk>
2021-06-04 00:07:36 +0100

    add bank account projects

---
+++ account.cmd.api/src/test/java/com/org/obtech/account/cmd/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.obtech.account.cmd.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
